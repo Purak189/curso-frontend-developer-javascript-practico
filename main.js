262fdd00d6cf7f12949870f9b72c8ed5d25511cd
Purak189 <106536136+Purak189@users.noreply.github.com>
2023-12-01 21:55:08 -0500

    agregado de uso de .json y productos

--- main.js
+++ main.js
@@ -30,47 +30,15 @@ productDetailClose.addEventListener('click', () => {
   shoppingCartContainer.classList.add('inactive');
 });
 
+async function cargarProductos() {
+  const response = await fetch('productos.json');
+  const data = await response.json();
+  return data;
+}
 
-
-const productList = [];
-productList.push({
-  name: 'Mouse Gamer Acer',
-  price: 120,
-  image: 'https://images.pexels.com/photos/19012038/pexels-photo-19012038/free-photo-of-tecnologia-moderno-raton-equipo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Dron',
-  price: 1400,
-  image: 'https://images.pexels.com/photos/19131319/pexels-photo-19131319/free-photo-of-drone-volando-de-cerca.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Mando PS4',
-  price: 450,
-  image: 'https://images.pexels.com/photos/1082810/pexels-photo-1082810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Teclado',
-  price: 700,
-  image: 'https://images.pexels.com/photos/1714205/pexels-photo-1714205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Macbook Pro',
-  price: 3400,
-  image: 'https://images.pexels.com/photos/577560/pexels-photo-577560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Iphone 12',
-  price: 2400,
-  image: 'https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Teclado Gamer',
-  price: 350,
-  image: 'https://images.pexels.com/photos/4792716/pexels-photo-4792716.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-
-function renderProducts(arr){
-  for (product of arr){
+async function renderProducts(){
+  const productos = await cargarProductos();
+  for (const product of productos){
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
   
@@ -106,4 +74,4 @@ function renderProducts(arr){
     cardContainer.appendChild(productCard);
   }
 }
-renderProducts(productList);
+renderProducts();
